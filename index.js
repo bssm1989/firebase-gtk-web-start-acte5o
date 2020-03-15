a9df1fcbb2cf631c750aff5bc833129050536118
bssm1989 <bssm1989@gmail.com>
2020-03-15 22:09:51 +0700

    2 files modified

--- index.js
+++ index.js
@@ -24,9 +24,19 @@ var rsvpListener = null;
 var guestbookListener = null;
 
 // Add Firebase project configuration object here
+const firebaseConfig = {
+  apiKey: "AIzaSyBQRJcqreTetu-twEtlO4kjcdMQO8VJL_U",
+  authDomain: "fir-web-codelab-de3c2.firebaseapp.com",
+  databaseURL: "https://fir-web-codelab-de3c2.firebaseio.com",
+  projectId: "fir-web-codelab-de3c2",
+  storageBucket: "fir-web-codelab-de3c2.appspot.com",
+  messagingSenderId: "809080271314",
+  appId: "1:809080271314:web:bc3f0acad4724b158b980b",
+  measurementId: "G-YM3PYNFT7R"
+};
 // var firebaseConfig = {};
 
-// firebase.initializeApp(firebaseConfig);
+ firebase.initializeApp(firebaseConfig);
 
 // FirebaseUI config
 const uiConfig = {
@@ -44,4 +54,48 @@ const uiConfig = {
   }
 };
 
-// const ui = new firebaseui.auth.AuthUI(firebase.auth());
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
+// At the bottom
+
+// Listen to RSVP button clicks
+startRsvpButton.addEventListener("click",
+ () => {
+      ui.start("#firebaseui-auth-container", uiConfig);
+});
+// Listen to the current Auth state
+firebase.auth().onAuthStateChanged((user)=> {
+  if (user) {
+    startRsvpButton.textContent = "LOGOUT"
+  }
+  else {
+    startRsvpButton.textContent = "RSVP"
+  }
+});
+// Called when the user clicks the RSVP button
+startRsvpButton.addEventListener("click",
+ () => {
+    if (firebase.auth().currentUser) {
+      // User is signed in; allows user to sign out
+      firebase.auth().signOut();
+    } else {
+      // No user is signed in; allows user to sign in
+      ui.start("#firebaseui-auth-container", uiConfig);
+    }
+});
+
+// Listen to the form submission
+form.addEventListener("submit", (e) => {
+ // Prevent the default form redirect
+ e.preventDefault();
+ // Write a new message to the database collection "guestbook"
+ firebase.firestore().collection("guestbook").add({
+   text: input.value,
+   timestamp: Date.now(),
+   name: firebase.auth().currentUser.displayName,
+   userId: firebase.auth().currentUser.uid
+ })
+ // clear message input field
+ input.value = ""; 
+ // Return false to avoid redirect
+ return false;
+});
